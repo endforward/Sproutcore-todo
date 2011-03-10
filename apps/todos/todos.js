3f7d86bd254358838abbe2ad93dc08f4b723c179
Olle de Zwart <olle@endforward.nl>
2011-03-10 23:01:36 +0100

    Todos list controller added

--- apps/todos/todos.js
+++ apps/todos/todos.js
@@ -17,3 +17,12 @@ jQuery(document).ready(function() {
     templateName: 'todos'
   });
 });
+
+Todos.todoListController = SC.ArrayController.create({
+  content: [],
+
+  createTodo: function(title) {
+    var todo = Todos.Todo.create({title: title});
+    this.pushObject(todo);
+  }
+});
